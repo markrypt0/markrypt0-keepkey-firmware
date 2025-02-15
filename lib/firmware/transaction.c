fb8b79c1cf47643b5c4b887c12ef5aa95877e81b
markrypt0 <cryptoakorn@gmail.com>
2025-02-15 10:44:24 -0700

    finish fw refactor and support docker build

--- lib/firmware/transaction.c
+++ lib/firmware/transaction.c
@@ -248,7 +248,18 @@ int compile_output(const CoinType *coin, const HDNode *root, TxOutputType *in,
           }
         }
       }
+
+#else  // for btc-only, don't do a thorchain memo check
+      else {
+        if (!confirm_data(ButtonRequestType_ButtonRequest_ConfirmOutput,
+                        _("Confirm OP_RETURN"), in->op_return_data.bytes,
+                        in->op_return_data.size)) {
+          return -1;  // user aborted
+        }
+      }
+
 #endif // BITCOIN_ONLY
+
     }
     uint32_t r = 0;
     out->script_pubkey.bytes[0] = 0x6A;
